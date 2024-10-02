105164d0ceb4f647118ba5ca24ae5955743d1bbf
Adamaski4 <adamaski4@tlen.pl>
2024-10-02 19:18:37 +0200

    Create main.py
    heeheh

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+import time
+
+facts = ["lubie grać w minecraft", " mam na imie Adam Choiński","cebula!", "mam lat 10","...","telleworanikaruta","lubie grać w minecraft", " mam na imie Adam Choiński","cebula!", "mam lat 10","lubie grać w minecraft", " mam na imie Adam Choiński","cebula!", "mam lat 10","telleworanikaruta","lubie grać w minecraft", " mam na imie Adam Choiński","cebula!", "mam lat 10","lubie grać w minecraft", " mam na imie Adam Choiński","cebula!", "mam lat 10","telleworanikaruta","lubie grać w minecraft","cebula!", "mam lat 10","lubie grać w minecraft","cebula!", "mam lat 10","telleworanikaruta","lubie grać w minecraft", " mam na imie Adam Choiński","cebula!", "mam lat 10"]
+
+while True :
+    x = random.randint(1,17)
+    print(facts[x])
+    time.sleep(1)
